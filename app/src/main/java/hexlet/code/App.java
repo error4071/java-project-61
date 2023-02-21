92b08f43a21b43661a110abacd1af3f2b83a5f46
error4071 <sergey_tsoy87@mail.ru>
2023-02-21 22:38:41 +0500

    App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,5 +3,8 @@ package hexlet.code;
 class App {
     public static void main(String[] arguments) {
         System.out.println("Welcome to the Brain Games!");
+        Cli.userName();
     }
 }
+
+
