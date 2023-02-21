a6b3a96801e9ca3d256209233e17fd98a97283ec
error4071 <sergey_tsoy87@mail.ru>
2023-02-21 23:36:46 +0500

    App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,10 +1,18 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 class App {
     public static void main(String[] arguments) {
+
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+
+        System.out.println("Please enter the game number and press Enter");
+        System.out.println("1 - Greet /n0 - Exit");
+        System.out.println("Your choice " + number );
+
         System.out.println("Welcome to the Brain Games!");
         Cli.userName();
     }
 }
-
-
